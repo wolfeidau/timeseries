555012dd8d516a3832240847127ab60f52480d94
Mark Wolfe <mark@wolfe.id.au>
2013-11-03 15:31:34 +1100

    Using to test out resource usage.

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 //
+// Simple test application which uses the tempodb golang library to
+// upload a entry ever 500ms for a period of time.
 //
 package main
 
 import (
+	"fmt"
 	"github.com/tempodb/tempodb-go"
 	"github.com/wolfeidau/timeseries/config"
 	"log"
+	"time"
 )
 
 func main() {
@@ -15,12 +19,31 @@ func main() {
 	client := tempodb.NewClient(settings.ApiKey, settings.ApiSecret)
 
 	// create the time series
-	created, err := client.CreateSeries("my-series3")
+	//	created, err := client.CreateSeries("my-series3")
 
-	if err != nil {
-		log.Fatal("Error occured creating timeseries %s", err)
-	}
+	ticker := time.NewTicker(time.Millisecond * 500)
 
-	log.Printf("timeseries %+v\n", created)
+	go func() {
+		for t := range ticker.C {
+			fmt.Println("sending data point at", t)
 
+			datapoints := []*tempodb.DataPoint{
+				&tempodb.DataPoint{
+					Ts: time.Now().UTC(),
+					V:  1.23,
+				},
+			}
+
+			err := client.WriteKey("1_D90343085872CA9C_687_0_2000", datapoints)
+
+			if err != nil {
+				log.Fatal("Error occured creating timeseries %s", err)
+				return
+			}
+
+		}
+	}()
+
+	fmt.Println("Writing data for 60 seconds")
+	time.Sleep(time.Second * 60)
 }
